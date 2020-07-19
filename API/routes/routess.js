4b606e8a5eeea0e0a85d6809cf3f738cb86e9839
Rajat Upadhyay <rajatupadhyay@Rajats-MacBook-Air.local>
2020-07-20 01:38:07 +0530

    update is admin org name org code to user master

--- API/routes/routess.js
+++ API/routes/routess.js
@@ -8,6 +8,7 @@ router.get('/get_userFromMobileNo',controller.get_userFromMobileNo);
 router.post('/post_updatePassword',controller.post_updatePassword);
 router.get('/get_Login',controller.get_Login);
 router.post('/post_organizationMaster',controller.post_organizationMaster);
+router.post('/post_updateAdmin',controller.post_updateAdmin);
 
 
 
